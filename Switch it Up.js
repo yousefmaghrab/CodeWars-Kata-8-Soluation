52c682783e72886bc03fc729f869956746def4f2
Yousef Maghraby <maghrabyyousef7@gmail.com>
2025-08-06 22:04:40 +0300

    new commit

---
+++ Switch it Up.js
@@ -0,0 +1,39 @@
+var num = Number(prompt('please Enter Number Between Number 0 to 9'))
+
+
+switch (num) {
+    case 0:
+        console.log("Zero")
+        break;
+    case 1:
+        console.log("One")
+        break;
+    case 2:
+        console.log("Two")
+        break;
+    case 3:
+        console.log("Three")
+        break;
+    case 4:
+        console.log("Four")
+        break;
+    case 5:
+        console.log("Five")
+        break;
+    case 6:
+        console.log("six")
+        break;
+    case 7:
+        console.log("Seven")
+        break;
+    case 8:
+        console.log("Eight")
+        break;
+    case 9:
+        console.log("Nine")
+        break;
+    default:
+        console.log("Please Enter Number Between 0 to 9")
+        var num = Number(prompt("enter num"))
+        break;
+}
